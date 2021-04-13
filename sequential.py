7cec5dc47999bd0e2265957404b7ee1d1ac2c05c
Minghui Liu <minghui.kevin.liu@gmail.com>
2021-04-13 12:12:23 +0800

    Add sequential.py

---
+++ sequential.py
@@ -0,0 +1,53 @@
+import random
+import heapq
+import math
+
+
+
+def reconstruct_path(prev, curr):
+    path = [curr]
+    while prev[curr]:
+        path.insert(0, prev[curr])
+        curr = prev[curr]
+    return path
+
+def l2_heuristic(curr, dest):
+    return math.sqrt((curr[0]-dest[0])**2 + (curr[1]-dest[1])**2)
+
+# Sequential A* Search
+def sequential_astar(G, N, src, dest, H):
+    """
+    A sequential implementation of A* search algorithm
+    Arguments:
+        G: the networkx graph
+        N: number of nodes
+        src: source node id
+        dest: destination node id
+        H: heuristic function, parameters (a, b) where a and b are coordinates in 2-tuple form
+    Returns:
+        (cost, path)
+    """
+    openSet = [(0, src)]
+    heapq.heapify(openSet)
+    closedSet = set()
+    costToCome = [float('inf') if i != src else 0 for i in range(N)]
+    prev = [None for i in range(N)]
+
+    while len(openSet) > 0:
+        _, curr = heapq.heappop(openSet)
+        if curr == dest:
+            return (costToCome[curr], reconstruct_path(prev, curr))
+            break
+        if curr in closedSet:
+            continue
+        closedSet.add(curr)
+        for nbr, attr in G.adj[curr].items():
+            if nbr in closedSet:
+                continue
+            gcost = costToCome[curr] + attr['weight']
+            fcost = gcost + l2_heuristic((G.nodes[nbr]['x'],G.nodes[nbr]['y']), (G.nodes[dest]['x'],G.nodes[dest]['y']))
+            if gcost > costToCome[nbr]:
+                continue
+            costToCome[nbr] = gcost
+            prev[nbr] = curr
+            heapq.heappush(openSet, (fcost, nbr))
