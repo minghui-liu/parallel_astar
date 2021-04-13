ea4f37481aeda7e828898ba8f39ac760256080b3
Minghui Liu <minghui.kevin.liu@gmail.com>
2021-04-13 16:16:03 +0800

    add dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,23 @@
+
+# Dijkstra's algorithm for comparison and correctness check
+def dijkstra(G, N, src, dest):
+    unvisited = set(range(N))
+    dist = [float('inf') for i in range(N)]
+    dist[src] = 0
+    prev = [None for i in range(N)]
+
+    while len(unvisited) > 0:
+        _, curr = min([(dist[i], i) for i in unvisited])
+        unvisited.remove(curr)
+        if curr == dest:
+            path = []
+            while curr:
+                path.insert(0, curr)
+                curr = prev[curr]
+            return (dist[dest], path)
+        for nbr, attr in G.adj[curr].items():
+            alt = dist[curr] + attr['weight']
+            if alt < dist[nbr]:
+                dist[nbr] = alt
+                prev[nbr] = curr
+    return (-1, [])
